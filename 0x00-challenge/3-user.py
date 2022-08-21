e7240f91b16eba2cb1bbc843e3e6121bc899fb0b
Adedayo Adepoju <1mole.coco@gmail.com>
2022-08-21 17:22:03 +0100

    Initial commit

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,9 +52,10 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
+
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
